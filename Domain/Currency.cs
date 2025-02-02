1da8939a589115cec85375f53047c7ad44f6ed2c
UHDFox <108611791+UHDFox@users.noreply.github.com>
2025-02-02 15:44:58 +0300

    Register client

--- Domain/Currency.cs
+++ Domain/Currency.cs
@@ -5,13 +5,13 @@ public sealed class Currency
     public string Id { get; set; }
     public string NumCode { get; set; }
     public string CharCode { get; set; }
-    public short Nominal { get; set; }
+    public int Nominal { get; set; }
 
     public string Name { get; set; }
     public decimal Value { get; set; }
     public decimal Previous { get; set; }
 
-    public Currency(string id, string numCode, string charCode, short nominal,string name, decimal value, decimal previous)
+    public Currency(string id, string numCode, string charCode, int nominal,string name, decimal value, decimal previous)
     {
         Id = id;
         NumCode = numCode;
@@ -21,4 +21,6 @@ public sealed class Currency
         Value = value;
         Previous = previous;
     }
+    
+    public Currency(){}
 }
