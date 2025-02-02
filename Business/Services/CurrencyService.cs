57302ffc7a727ff6d75553441f8ce79be272db83
UHDFox <108611791+UHDFox@users.noreply.github.com>
2025-02-02 18:11:45 +0300

    Add currency caching

--- Business/Services/CurrencyService.cs
+++ Business/Services/CurrencyService.cs
@@ -2,6 +2,7 @@ using AutoMapper;
 using Client;
 using Domain;
 using Domain.Enums;
+using Microsoft.Extensions.Caching.Memory;
 
 namespace Business.Services;
 
@@ -9,21 +10,44 @@ public sealed class CurrencyService : ICurrencyService
 {
     private readonly CurrencyClient _currencyClient;
     private readonly IMapper _mapper;
+    private readonly IMemoryCache _memoryCache;
 
-    public CurrencyService(CurrencyClient currencyClient, IMapper mapper)
+    public CurrencyService(CurrencyClient currencyClient, IMapper mapper, IMemoryCache memoryCache)
     {
         _currencyClient = currencyClient;
         _mapper = mapper;
+        _memoryCache = memoryCache;
     }
     public async Task<IReadOnlyCollection<Currency>> GetCurrencyListAsync(int offset, int limit)
     {
-        return  await _currencyClient.GetCurrencyListAsync( offset, limit);
+        var cacheKey = $"CurrencyListPage-{offset}-{limit}";
+
+        if (!_memoryCache.TryGetValue(cacheKey, out IReadOnlyCollection<Currency> currencies))
+        {
+            var currencyList = await _currencyClient.GetCurrencyListAsync(offset, limit);
+            
+            var cacheOpts = new MemoryCacheEntryOptions()
+                .SetSlidingExpiration(TimeSpan.FromHours(24)); // цб обновляет данные раз в сутки
+
+            _memoryCache.Set(cacheKey, currencyList, cacheOpts);
+        }
+        return currencies;
     }
 
     public async Task<Currency> GetCurrencyByCodeAsync(CurrencyCode code)
     {
-        var result = await _currencyClient.GetCurrencyByCodeAsync(code);
+        var cacheKey = $"currency:{code}";
         
-        return _mapper.Map<Currency>(result);
+        if (!_memoryCache.TryGetValue(cacheKey, out Currency currency))
+        {
+            currency = _mapper.Map<Currency>(await _currencyClient.GetCurrencyByCodeAsync(code));
+            
+            var cacheOpts = new MemoryCacheEntryOptions()
+                .SetSlidingExpiration(TimeSpan.FromHours(24)); // цб обновляет данные раз в сутки
+            
+            _memoryCache.Set(cacheKey, currency, cacheOpts);
+        }
+        
+        return currency;
     }
 }
